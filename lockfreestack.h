a791b35de05c609fe1ba91cd93d688ca44568ce5
ASProgrammer <alexeyseledtsov@yandex.ru>
2015-09-22 21:54:34 +0300

    added gc for stack

--- lockfreestack.h
+++ lockfreestack.h
@@ -20,14 +20,22 @@ class LockfreeStack {
 
 public:
     LockfreeStack():
-        m_head { nullptr }
+        m_head { nullptr },
+        m_deleted_nodes { nullptr },
+        m_threads_in_pop { 0 }
     { }
 
     void push(const T& new_value);
     std::shared_ptr<T> pop();
 
 private:
+    void try_reclaim(Node* old_head);
+    void delete_nodes(Node* list);
+
+private:
     std::atomic<Node*> m_head;
+    std::atomic<Node*> m_deleted_nodes;
+    std::atomic<int> m_threads_in_pop;
 };
 
 template <typename T>
@@ -41,9 +49,41 @@ void LockfreeStack<T>::push(const T& new_value)
 template <typename T>
 std::shared_ptr<T> LockfreeStack<T>::pop()
 {
+    m_threads_in_pop.fetch_add(1);
+
     Node* old_head = m_head.load();
     while (old_head && !m_head.compare_exchange_weak(old_head, old_head->m_next));
-    return old_head ? old_head->m_data : nullptr;
+
+    auto res_data = old_head ? old_head->m_data : nullptr;
+    try_reclaim(old_head);
+
+    return res_data;
+}
+
+template <typename T>
+void LockfreeStack<T>::try_reclaim(Node* old_head)
+{
+    if (m_threads_in_pop == 1) {
+        auto nodes = m_deleted_nodes.exchange(nullptr);
+        if (!--m_threads_in_pop) {
+            delete_nodes(nodes);
+        }
+        else if (nodes) {
+
+        }
+
+        delete old_head;
+    }
+}
+
+template <typename T>
+void LockfreeStack<T>::delete_nodes(Node* list)
+{
+    while (list) {
+        Node* next = list->m_next;
+        delete list;
+        list = next;
+    }
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include <lockfreestack.h>
+#include "lockfreestack.h"
 
 int main(int argc, char** argv) try {
     lockfree::LockfreeStack<int> stack;
